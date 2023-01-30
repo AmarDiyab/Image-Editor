56bb2bf6291efed9578f9d760954e1fadbdda5e5
Amar Diyab <diyabamar3043@gmail.com>
2023-01-30 21:04:20 +0530

    fixes

--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@ import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
   <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
   </Provider>
+  </React.StrictMode>
 );
 
